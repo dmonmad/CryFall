48ac07b663d17652ae34a72ce115e74817eae81c
Juan Antonio Cabello <juanantonio186@hotmail.com>
2020-03-05 15:47:45 +0100

    selector nivel en menú

---
+++ Assets/ButtonHandler.cs
@@ -0,0 +1,28 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonHandler : MonoBehaviour{
+    static int selector = 0;
+    public void NivelDerecha()
+    {
+
+        if (selector < SceneManager.sceneCountInBuildSettings)
+        {
+            selector++;
+            TextMeshProUGUI t = GameObject.Find("Canvas/NivelText").GetComponent<TextMeshProUGUI>();
+            t.SetText(SceneManager.GetSceneAt(selector).name);
+        }
+
+    }
+
+    public void NivelIzquierda()
+    {
+        if (selector > 0)
+        {
+            selector--;
+            TextMeshProUGUI t = GameObject.Find("Canvas/NivelText").GetComponent<TextMeshProUGUI>();
+            t.SetText(SceneManager.GetSceneAt(selector).name);
+        }
+    }
+}
